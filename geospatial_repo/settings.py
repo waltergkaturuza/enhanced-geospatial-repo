86b345bd5990e4f74535c9e7b8823ccfb6894dbf
Walter Katuruza <waltergkaturuza@gmail.com>
2025-10-27 10:40:25 +0200

    Add frontend domain to CORS allowed origins for cross-domain communication

--- geospatial_repo/settings.py
+++ geospatial_repo/settings.py
@@ -478,6 +478,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
+    "https://enhanced-geospatial-repo-1.onrender.com",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
